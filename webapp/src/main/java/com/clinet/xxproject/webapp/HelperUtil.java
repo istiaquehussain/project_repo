0b565446f9ee59350614aa7760c431c42aa2dd67
istiaque hussain <istiaque_hussain@infosys.com>
2014-04-11 16:19:31 +0530

    commit webapp 02

---
+++ webapp/src/main/java/com/clinet/xxproject/webapp/HelperUtil.java
@@ -0,0 +1,11 @@
+package com.clinet.xxproject.webapp;
+import com.clinet.xxproject.coreframework.*;
+
+public class HelperUtil {
+	public String getUserDetils()
+	{
+		FrameworkUtil MyUtil = new FrameworkUtil();
+		return MyUtil.getData();
+	}
+
+}
